355c1f839914d80b77dddfea82fa01c4f0f77fde
Radfoxmelvin <141583445+Radfoxmelvin@users.noreply.github.com>
2023-10-20 19:13:56 +0200

    Just before intro to recycler view

--- app/src/main/java/com/example/clothing/Controller/MainActivity.kt
+++ app/src/main/java/com/example/clothing/Controller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.clothing.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 //import android.widget.ArrayAdapter
 import com.example.clothing.Adapters.CategoryAdapter
 //import com.example.clothing.R
@@ -30,6 +31,12 @@ class MainActivity : AppCompatActivity() {
         adapter=CategoryAdapter(this,DataServices.categories)
          setContentView(binding.root)
          binding.categoryListView.adapter=adapter
+
+        binding.categoryListView.setOnItemClickListener{adapterView,view,i,l->
+
+            val category = DataServices.categories[i]
+            Toast.makeText(this,"You have clicked on ${category.title} cell",Toast.LENGTH_SHORT).show()
+        }//(Iyi inondzi LIST VIEW method, but iyihayizi prefered method over RECYCLER VIEW METHOD
 
         }
 
